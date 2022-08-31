ed089bfed2469f0c3fa0d880619ec3df1768f9b6
Miguel Cardoso <6021036+mncardoso@users.noreply.github.com>
2022-08-31 20:08:49 +0100

    woring version

--- src/pages/[iso].tsx
+++ src/pages/[iso].tsx
@@ -4,119 +4,113 @@ import { Vaccinations } from "@/components/graphs/Vaccinations";
 import { MenuBar } from "@/layout/MenuBar";
 import Head from "next/head";
 
-const Iso = () =>
-	// 	props: {
-	// 	location: string;
-	// 	population: number;
-	// 	data: {
-	// 		new_cases: number;
-	// 		new_deaths: number;
-	// 		date: string;
-	// 		new_cases_smoothed: number;
-	// 		new_deaths_smoothed: number;
-	// 		reproduction_rate: number;
-	// 		people_vaccinated: number;
-	// 		people_fully_vaccinated: number;
-	// 		total_boosters: number;
-	// 	}[];
-	// 	menuData: any;
-	// }
-	{
-		return <>1</>;
-		// return (
-		// 	<div>
-		// 		<Head>
-		// 			<title>{`Covid Dashboard | ${props.location}`}</title>
-		// 		</Head>
-		// 		<MenuBar data={props.menuData} />
-		// 		<main className="charts">
-		// 			<div className="title">
-		// 				<h1>{`${props.location}`}</h1>
-		// 			</div>
-		// 			<div className="cases section">
-		// 				<h3>Covid Cases</h3>
-		// 				<div className="content" id="new_cases">
-		// 					<BarChart
-		// 						data={props.data}
-		// 						population={props.population}
-		// 						id="new_cases"
-		// 						dateFromat="%m/%y"
-		// 						term="new_cases"
-		// 						death={false}
-		// 						t14={false}
-		// 					/>
-		// 				</div>
-		// 			</div>
-		// 			<div className="stats section" id="stats">
-		// 				<h3>Covid Stats</h3>
-		// 				<div className="content">
-		// 					<CovidStats
-		// 						data={props.data.reverse()}
-		// 						population={props.population}
-		// 					/>
-		// 				</div>
-		// 			</div>
-		// 			<div className="lastdays section">
-		// 				<h3>Last 14 days</h3>
-		// 				<div className="content" id="last_days">
-		// 					<BarChart
-		// 						data={props.data}
-		// 						population={props.population}
-		// 						id="last_days"
-		// 						dateFromat="%d"
-		// 						term="new_cases"
-		// 						death={false}
-		// 						t14={true}
-		// 					/>
-		// 				</div>
-		// 			</div>
-		// 			<div className="deaths section">
-		// 				<h3>Covid Deaths</h3>
-		// 				<div className="content" id="new_deaths">
-		// 					<BarChart
-		// 						data={props.data}
-		// 						population={props.population}
-		// 						id="new_deaths"
-		// 						dateFromat="%m/%y"
-		// 						term="new_deaths"
-		// 						death={true}
-		// 						t14={false}
-		// 					/>
-		// 				</div>
-		// 			</div>
-		// 			<div className="vaccinations section">
-		// 				<h3>Vaccinations</h3>
-		// 				<div className="content">
-		// 					<Vaccinations data={props.data} population={props.population} />
-		// 				</div>
-		// 			</div>
-		// 		</main>
-		// 	</div>
-		// );
-	};
+const Iso = (props: {
+	location: string;
+	population: number;
+	data: {
+		new_cases: number;
+		new_deaths: number;
+		date: string;
+		new_cases_smoothed: number;
+		new_deaths_smoothed: number;
+		reproduction_rate: number;
+		people_vaccinated: number;
+		people_fully_vaccinated: number;
+		total_boosters: number;
+	}[];
+	menuData: any;
+}) => {
+	return (
+		<div>
+			<Head>
+				<title>{`Covid Dashboard | ${props.location}`}</title>
+			</Head>
+			<MenuBar data={props.menuData} />
+			<main className="charts">
+				<div className="title">
+					<h1>{`${props.location}`}</h1>
+				</div>
+				<div className="cases section">
+					<h3>Covid Cases</h3>
+					<div className="content" id="new_cases">
+						<BarChart
+							data={props.data}
+							population={props.population}
+							id="new_cases"
+							dateFromat="%m/%y"
+							term="new_cases"
+							death={false}
+							t14={false}
+						/>
+					</div>
+				</div>
+				<div className="stats section" id="stats">
+					<h3>Covid Stats</h3>
+					<div className="content">
+						<CovidStats
+							data={props.data.reverse()}
+							population={props.population}
+						/>
+					</div>
+				</div>
+				<div className="lastdays section">
+					<h3>Last 14 days</h3>
+					<div className="content" id="last_days">
+						<BarChart
+							data={props.data}
+							population={props.population}
+							id="last_days"
+							dateFromat="%d"
+							term="new_cases"
+							death={false}
+							t14={true}
+						/>
+					</div>
+				</div>
+				<div className="deaths section">
+					<h3>Covid Deaths</h3>
+					<div className="content" id="new_deaths">
+						<BarChart
+							data={props.data}
+							population={props.population}
+							id="new_deaths"
+							dateFromat="%m/%y"
+							term="new_deaths"
+							death={true}
+							t14={false}
+						/>
+					</div>
+				</div>
+				<div className="vaccinations section">
+					<h3>Vaccinations</h3>
+					<div className="content">
+						<Vaccinations data={props.data} population={props.population} />
+					</div>
+				</div>
+			</main>
+		</div>
+	);
+};
 
 export default Iso;
 
-// const getBaseUrl = () => {
-// 	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-// 	return `http://localhost:${process.env.PORT ?? 3000}`;
-// };
+export async function getStaticPaths() {
+	const response = await fetch("https://covid-dashboard.app/api/owid");
+	const data = await response.json();
+	const paths = Object.keys(data).map((iso: string) => ({
+		params: { iso: iso },
+	}));
+	return { paths, fallback: "blocking" };
+}
 
-// export async function getStaticPaths() {
-// 	const response = await fetch(`${getBaseUrl()}/api/owid`);
-// 	const data = await response.json();
-// 	const paths = Object.keys(data).map((iso: string) => ({
-// 		params: { iso: iso },
-// 	}));
-// 	return { paths, fallback: "blocking" };
-// }
-
-// export async function getStaticProps({ params }: { params: { iso: string } }) {
-// 	const response = await fetch(`${getBaseUrl()}/api/owid`);
-// 	const menuData = await response.json();
-// 	const location = menuData[params.iso]?.location;
-// 	const population = menuData[params.iso]?.population;
-// 	const responseISO = await fetch(`${getBaseUrl()}/api/owid/${params.iso}`);
-// 	const data = await responseISO.json();
-// 	return { props: { location, population, data, menuData }, revalidate: 3600 };
-// }
+export async function getStaticProps({ params }: { params: { iso: string } }) {
+	const response = await fetch("https://covid-dashboard.app/api/owid");
+	const menuData = await response.json();
+	const location = menuData[params.iso]?.location;
+	const population = menuData[params.iso]?.population;
+	const responseISO = await fetch(
+		`https://covid-dashboard.app/api/owid/${params.iso}`
+	);
+	const data = await responseISO.json();
+	return { props: { location, population, data, menuData }, revalidate: 3600 };
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,30 +2,19 @@ import { MenuBar } from "@/layout/MenuBar";
 import styles from "@/styles/index.module.css";
 import type { InferGetStaticPropsType } from "next";
 
-const Home = () =>
-	// 	{
-	// 	data,
-	// 	url,
-	// }: InferGetStaticPropsType<typeof getStaticProps>
-	{
-		return (
-			<>
-				{/* <MenuBar data={data} /> */}
-				<h1 className={styles.mid}>Welcome, please select a country</h1>
-			</>
-		);
-	};
+const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+	return (
+		<>
+			<MenuBar data={data} />
+			<h1 className={styles.mid}>Welcome, please select a country</h1>
+		</>
+	);
+};
 
 export default Home;
 
-const getBaseUrl = () => {
-	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-	return `http://localhost:${process.env.PORT ?? 3000}`;
-};
-
-// export async function getStaticProps() {
-// 	const url = `${getBaseUrl()}/api/trpc`;
-// 	const response = await fetch(`${getBaseUrl()}/api/owid`);
-// 	const data = await response.json();
-// 	return { props: { data, url }, revalidate: 3600 };
-// }
+export async function getStaticProps() {
+	const response = await fetch("https://covid-dashboard.app/api/owid");
+	const data = await response.json();
+	return { props: { data }, revalidate: 3600 };
+}
